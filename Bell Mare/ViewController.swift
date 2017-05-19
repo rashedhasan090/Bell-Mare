f0c42c9537074a6d05a2dd8d6e2e52c76a897465
Rashed Hasan <rashedhasan090@icloud.com>
2017-05-19 08:23:53 +0600

    Initial Commit

---
+++ Bell Mare/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bell Mare
+//
+//  Created by macbookair on 5/19/17.
+//  Copyright © 2017 diuappcontest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
